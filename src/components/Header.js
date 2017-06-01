56a271261cf01828f8088ec9c411e258c71e02fb
Ciprianna <ciprianna17@gmail.com>
2017-05-31 22:05:55 -0500

    enable sass hot reloading

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import styles from './App.css';
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div className={styles.header}>
+                <h2>{this.props.header}</h2>
+            </div>
+        );
+    }
+}
